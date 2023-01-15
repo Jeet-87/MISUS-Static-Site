f61950961a897cc4d4198f5ee8bae92b45108e6a
Prasenjit <52097988+Jeet-87@users.noreply.github.com>
2023-01-15 21:00:06 +0530

    started adding media queries

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-
-
 document.getElementById("connect-btn").addEventListener("click", () => {
     console.log("Button Clicked!");
 });
+
+window.addEventListener("scroll", () => {
+    console.log("Scrolled!");
+});
+
+// window.onscroll =  () => {
+//     console.log("Scrolled!");
+// }
